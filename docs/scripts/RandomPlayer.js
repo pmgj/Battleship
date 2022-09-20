e5936508084100b7a1df7d2f74d5a3be624ecb6c
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2022-09-20 11:13:32 -0300

    Improve random player

--- docs/scripts/RandomPlayer.js
+++ docs/scripts/RandomPlayer.js
@@ -8,19 +8,22 @@ export default class RandomPlayer {
     play() {
         let rows = this.grid.length;
         let cols = this.grid[0].length;
-        let x, y;
-        do {
-            x = this.getRandomInt(0, rows - 1);
-            y = this.getRandomInt(0, cols - 1);
-        } while (this.grid[x][y] !== State.NONE);
-        return new Cell(x, y);
+        let array = this.grid.flat();
+        let index = array.indexOf(State.NONE, this.getRandomInt(0, rows * cols - 1));
+        if (index === -1) {
+            index = array.indexOf(State.NONE);
+            if (index === -1) {
+                throw new Error("The grid does not have any empty spaces.");
+            }
+        }
+        return new Cell(Math.floor(index / cols), index % cols);
     }
     getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
-    setResultShot({x, y}, shot) {
+    setResultShot({ x, y }, shot) {
         this.grid[x][y] = shot;
     }
 }
